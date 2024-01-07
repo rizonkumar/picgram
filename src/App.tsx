37a7cf9b4a90fc87213d2cddd4555a2dcb9f2048
rizonkumar <rizon.kumar.rahi@gmail.com>
2024-01-07 10:55:27 +0530

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
+import "./globals.css";
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route path="/sign-in" element={<SigninForm />} />
+
+        {/* private routes */}
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
